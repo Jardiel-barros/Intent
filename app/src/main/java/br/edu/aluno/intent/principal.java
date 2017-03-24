387919c65bb184b1032c2a287e75a31490fb7505
aluno <jardiel_antonio@hotmail.com>
2017-03-24 20:47:48 -0300

    Criação de botão

--- app/src/main/java/br/edu/aluno/intent/principal.java
+++ app/src/main/java/br/edu/aluno/intent/principal.java
@@ -2,8 +2,12 @@ package br.edu.aluno.intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class principal extends AppCompatActivity {
+
+    private Button botaoUm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
